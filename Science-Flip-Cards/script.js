633de16b836adf2f37f54a0b27919aa70d430551
Emilio LG <milio.code@gmail.com>
2026-07-13 15:06:51 +0100

    Updated the html, css and JS files

--- Science-Flip-Cards/script.js
+++ Science-Flip-Cards/script.js
@@ -1,1 +1,36 @@
-// Coming soon...
+const cards = document.querySelectorAll(".card")
+
+
+cards.forEach(card => {
+  const cardParent = card.closest(".card")
+  const frontFace = cardParent.children[0]
+  const backFace = cardParent.children[1]
+
+  card.addEventListener('click', e => {
+    isNotFlipped()
+    if (e.target === frontFace && frontFace.style.transform === "") {
+      frontFace.style.transform = "rotateY(-180deg)"
+      backFace.style.transform = "rotateY(0deg)"
+      card.style.transform = "translateY(-1rem)"
+    } else {
+      clearStyle(card)
+    }
+  })
+})
+
+
+/* Flipped back when another card is clicked */
+function isNotFlipped() {
+  cards.forEach(card => {
+    clearStyle(card)
+  })
+}
+
+
+function clearStyle(card) {
+  card.closest(".card").children[0].style.transform = ""
+  card.closest(".card").children[1].style.transform = ""
+  card.style.transform = ""
+}
+
+/* Some changes will come soon*/
