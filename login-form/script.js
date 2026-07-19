b87e8f1a028a891662c1943c244d38ab8dd95c82
Emilio LG <milio.code@gmail.com>
2026-07-19 11:14:36 +0100

    updating js file

--- login-form/script.js
+++ login-form/script.js
@@ -1,17 +1,47 @@
 const form = document.querySelector("#form")
-const submitBtn = document.querySelector("#submit")
+const username = document.querySelector("#username")
+const email = document.querySelector("#email")
+const password = document.querySelector("#password")
+const passwdConf = document.querySelector("#password-conf")
+// const submitBtn = document.querySelector("#submit")
 
 
-form.addEventListener("click", e => {
+form.addEventListener("submit", e => {
   e.preventDefault()
+  checkRequired([username, email, password, passwdConf])
+  checkLength(username, 3, 15)
+  checkLength(password, 8, 25)
 })
 
 
-function isValid() {
+function checkRequired(arrayInput) {
+  arrayInput.forEach(input => (input.value.trim() === "") ? showError(input, `${nameCapitalized(input.id)} is required`) : showSuccess(input));
+}
+
+function checkLength(input, min, max) {
 
 }
 
+function nameCapitalized(input) {
+  return `${input.slice(0, 1).toUpperCase()}${input.slice(1)}`
+}
 
-function showErrors() {
 
+function isEmailValid(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase())
+}
+
+
+function showError(input, message) {
+  const formControl = input.closest(".box")
+  formControl.className = "box invalid"
+  const errorMessage = formControl.querySelector(".error-message")
+  errorMessage.textContent = message
+}
+
+
+function showSuccess(input) {
+  const formControl = input.closest(".box")
+  formControl.className = "box valid"
 }
