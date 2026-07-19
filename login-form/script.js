286bfcf531d0c4dd350dc9056acd568d406c1559
Emilio LG <milio.code@gmail.com>
2026-07-19 01:25:37 +0100

    updated html and css files

---
+++ login-form/script.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("#form")
+const submitBtn = document.querySelector("#submit")
+
+
+form.addEventListener("click", e => {
+  e.preventDefault()
+})
+
+
+function isValid() {
+
+}
+
+
+function showErrors() {
+
+}
